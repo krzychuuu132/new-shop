7adec100a59411517992dcb41a41b8c88422f1da
krzysztof <krzychu222u@wp.pl>
2021-11-21 21:42:59 +0100

    changing technology to React.js

--- src/helpers/apollo.js
+++ src/helpers/apollo.js
@@ -1,7 +1,7 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
-  uri: "http://krzysztofk.ct8.pl/graphql",
+  uri: 'http://krzysztofk.ct8.pl/graphql',
   cache: new InMemoryCache(),
 });
 
